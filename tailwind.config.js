e737008673e68e96588becbced3de7f6c6b3d00c
Shivendra-Pandey <99125756+Shivendra-Pandey@users.noreply.github.com>
2023-03-31 16:27:28 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'roboto': ['Roboto', 'sans-serif'],
+        'poppins' : ['Poppins' , 'sans-serif'],
+        'cabin' : ['Cabin Sketch', 'cursive'],
+      }
+    },
+  },
+  plugins: [],
+}
